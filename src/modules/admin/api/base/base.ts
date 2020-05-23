c3db0c8bb7fd185f8ecd0d92ab254c912611f2eb
nductung <ductung1096@gmail.com>
2020-05-23 13:37:32 +0700

    update

--- src/modules/admin/api/base/base.ts
+++ src/modules/admin/api/base/base.ts
@@ -1,23 +1,9 @@
 import {InterfaceModelAuth} from "../../../../models/auth.model";
 import AdminAuthenticationService from "../v1/services/admin.authentication.service";
-import {AdminCategoryService} from '../v1/services/admin.category.service';
-import {AdminCommentService} from '../v1/services/admin.comment.service';
-import {AdminHashtagService} from '../v1/services/admin.hashtag.service';
-import {AdminPostService} from '../v1/services/admin.post.service';
-import {AdminProxyService} from '../v1/services/admin.proxy.service';
-import {AdminUserService} from '../v1/services/admin.user.service';
-import {AdminJobService} from '../v1/services/admin.job.service';
 
 export class Base {
     public globals: any = global;
     public adminAuthenticationService = new AdminAuthenticationService();
-    public adminCategoryService = new AdminCategoryService();
-    public adminCommentService = new AdminCommentService();
-    public adminHashtagService = new AdminHashtagService();
-    public adminPostService = new AdminPostService();
-    public adminProxyService = new AdminProxyService();
-    public adminUserService = new AdminUserService();
-    public adminJobService = new AdminJobService();
 
     public getProfile = (): InterfaceModelAuth => this.globals.user;
 
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,6 @@ import App from './app';
 import validateEnv from './utils/validateEnv';
 import AuthenticationController from "./modules/api/v1/controllers/authentication.controller";
 import AdminAuthenticationController from "./modules/admin/api/v1/controllers/admin.authentication.controller";
-import AdminJobController from './modules/admin/api/v1/controllers/admin.job.controller';
-import AdminPostController from './modules/admin/api/v1/controllers/admin.post.controller';
-import AdminUserController from './modules/admin/api/v1/controllers/admin.user.controller';
-import AdminProxyController from './modules/admin/api/v1/controllers/admin.proxy.controller';
-import AdminCategoryController from './modules/admin/api/v1/controllers/admin.category.controller';
-import AdminHashtagController from './modules/admin/api/v1/controllers/admin.hashtag.controller';
-import AdminCommentController from './modules/admin/api/v1/controllers/admin.comment.controller';
 
 validateEnv();
 
@@ -20,13 +13,7 @@ const app = new App(
 
         // Admin
         new AdminAuthenticationController(),
-        new AdminJobController(),
-        new AdminPostController(),
-        new AdminUserController(),
-        new AdminCommentController(),
-        new AdminProxyController(),
-        new AdminCategoryController(),
-        new AdminHashtagController(),
+
     ],
 );
 
