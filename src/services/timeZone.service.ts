0b535c5dac60cb5d464d2f56c14a3ddbfe57a85a
nductung <ductung1096@gmail.com>
2020-07-02 15:21:36 +0700

    update

--- src/services/timeZone.service.ts
+++ src/services/timeZone.service.ts
@@ -2,9 +2,13 @@ import * as moment from "moment-timezone";
 
 export class TimeZoneService {
 
-    public static convertTimeZoneVietnam(date: any) {
+    public static getCurrentDate() {
+        return moment().tz("Atlantic/Azores").format();
+    }
+
+    public static getDateTimezoneVietnam(date: any) {
         date = moment(date);
-        return date.tz("Asia/Ho_Chi_Minh").format();
+        return date.tz("Asia/Ho_Chi_Minh").format()
     }
 
 }
