ba5391738d5b1d79f251dcd220f37edb469a85ee
nductung <ductung1096@gmail.com>
2020-07-02 18:19:36 +0700

    update-session-login

--- src/exceptions/WrongAuthenticationTokenException.ts
+++ src/exceptions/WrongAuthenticationTokenException.ts
@@ -2,7 +2,8 @@ import HttpException from './HttpException';
 
 class WrongAuthenticationTokenException extends HttpException {
     constructor() {
-        super(401, 'Wrong authentication token');
+        // super(401, 'Wrong authentication token');
+        super(401, 'Mã xác thực không hợp lệ hoặc bị thiếu');
     }
 }
 
