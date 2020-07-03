4b749244760b865187e6f7bb1f89db43d9dd7a0c
nductung <ductung1096@gmail.com>
2020-07-03 08:33:21 +0700

    update

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -3,7 +3,7 @@ import * as jwt from "jsonwebtoken";
 class TokenService {
 
     public createToken(user: any, type: boolean) {
-        const expiresIn = type ? 60 * (60 * 6) : 60 * (60 * 24);
+        const expiresIn = type ? 60 * 30 : 60 * (60 * 24);
         const secret: string = process.env.JWT_SECRET;
         const dataStoredInToken: { _id: string; role: string } = {
             _id: user.id,
