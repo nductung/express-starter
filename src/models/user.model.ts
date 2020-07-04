b9c65bbc047bc0190b9763f7196575c86fa9ed01
nductung <ductung1096@gmail.com>
2020-07-04 16:00:14 +0700

    update

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
         password: {type: String, required: true},
         role: {type: String, default: 'user'},
         picture: {type: String, default: undefined},
-        ref_facebook: {type: String, default: undefined},
+        ref_facebook: {type: Number, default: undefined},
         createdAt: {type: Date, default: Date.now},
         updatedAt: {type: Date, default: Date.now}
     }, {
