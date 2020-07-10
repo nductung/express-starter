37ced7de47432ebc288578f85fcc2209e78dd1bd
nductung <ductung1096@gmail.com>
2020-07-10 16:20:48 +0700

    update

--- src/exceptions/AccountNotActiveException.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from './HttpException';
-
-class AccountNotActiveException extends HttpException {
-    constructor() {
-        super(400, 'Vui lòng kích hoạt tài khoản của bạn trước khi đăng nhập');
-    }
-}
-
-export default AccountNotActiveException;
