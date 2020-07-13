4ae4cd81e9ad9d90de1b675127730c78ec676a27
nductung <ductung1096@gmail.com>
2020-07-13 13:43:06 +0700

    update

--- src/services/sendMail.service.ts
+++ src/services/sendMail.service.ts
@@ -1,10 +1,8 @@
 import * as mailer from "nodemailer"
 import InterfaceModelUser from "../interfaces/user.interface";
-import TokenService from "./token.service";
 
 class SendMailService {
 
-    public tokenService = new TokenService();
     public globals: any = global;
 
     public sendMail(mailTo: any, message: string, subject: string) {
@@ -60,33 +58,3 @@ class SendMailService {
 }
 
 export default SendMailService
-
-// export default async function SendEmailService(mailTo: any, message: string, subject: string) {
-//     const port: number = parseInt(process.env.MAIL_PORT ? process.env.MAIL_PORT.toString() : "587", 10);
-//     const host: string = process.env.MAIL_HOST || "smtp.mailtrap.io";
-//     const transporter = mailer.createTransport({
-//         host,
-//         port,
-//         auth: {
-//             user: process.env.MAIL_USER || "933d9df2b87792",
-//             pass: process.env.MAIL_PASSWORD || "c9facdc8f6bd1e",
-//         }
-//     });
-//     const mailOptions = {
-//         from: "cs@minet.asia",
-//         to: mailTo,
-//         subject,
-//         html: message
-//     };
-//
-//     return new Promise(async (resolve, reject) => {
-//         transporter.sendMail(mailOptions, (error: any) => {
-//             if (error) {
-//                 console.log(error);
-//                 resolve(false)
-//             } else {
-//                 resolve(true)
-//             }
-//         })
-//     });
-// }
