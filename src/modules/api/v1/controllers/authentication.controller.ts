b4e762ac71ee5ea9840a29308dac2c9d01ba9d4b
nductung <ductung1096@gmail.com>
2020-07-16 09:34:42 +0700

    update

--- src/modules/api/v1/controllers/authentication.controller.ts
+++ src/modules/api/v1/controllers/authentication.controller.ts
@@ -38,6 +38,8 @@ export default class AuthenticationController extends ControllerBase implements 
 
     private initializeRoutes = () => {
         this.router
+            .get(`/`, (request: Request, response: Response) => response.send('Hello World!'))
+
             .post(`${this.path}/authentication/register`, validationMiddleware(RegisterDto), this.registration)
             .post(`${this.path}/authentication/login`, validationMiddleware(LoginDto), this.loggingIn)
             .get(`${this.path}/current`, authMiddleware(Role.User), this.getCurrent)
